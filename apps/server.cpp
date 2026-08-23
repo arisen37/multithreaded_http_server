721ec006b1dbee50edaf93c0d4c03b0ce44aa73e
arisen37 <arisen2356@gmail.com>
2026-08-23 05:36:03 +0530

    multithreaded tcp socket - without error handling

--- apps/server.cpp
+++ apps/server.cpp
@@ -1,14 +1,28 @@
 #include <http_server/socket_util.h>
+#include <thread>
+#include <mutex>
+
+std::mutex cout_lock;
+
+void AcceptTCPConnection(int clientSocket){
+    char buffer[1024] = {0};
+    recv(clientSocket , buffer , sizeof(buffer) , 0);
+    cout_lock.lock();
+    std::cout << "Message from " << clientSocket << " " << buffer << std::endl;
+    cout_lock.unlock();
+}
 
 int main(){
     int socketId = socket_util::createAndBindSocket(8080);
-    listen(socketId , 5);
+    listen(socketId , 10);
 
-    //Try TCP Socket
-    int clientSocket = accept(socketId , nullptr , nullptr);
-    char buffer[1024] = {0};
-    recv(clientSocket , buffer , sizeof(buffer) , 0);
-    std::cout << "Message from client : " << buffer << std::endl;
+    while(true){
+        //KEEP Listening for new connections
+        int clientSocket = accept(socketId, nullptr , nullptr);
+        std::thread worker(AcceptTCPConnection , clientSocket);
+
+        worker.detach();
+    }
 
     close(socketId);
 }
