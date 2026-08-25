db970b14525d37ed2a82a3f9fb3955e8cffd1620
arisen37 <arisen2356@gmail.com>
2026-08-25 23:55:50 +0530

    error handling

--- src/socket_util.cpp
+++ src/socket_util.cpp
@@ -1,6 +1,6 @@
 #include <http_server/socket_util.h>
 
-int socket_util::createAndBindSocket(int PORT){
+int socket_util::createAndBindSocket(int PORT1){
 
     //AF_INET -> IPV4 , SOCK_STREAM -> TCP Socket , 0 -> OS automatically selects protocol for the domain and socket type
     int serverSocket = socket(AF_INET , SOCK_STREAM , 0);
@@ -8,7 +8,7 @@ int socket_util::createAndBindSocket(int PORT){
     //sockaddr_in is the class used to store serverAddress
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(PORT);
+    serverAddress.sin_port = htons(PORT1);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
     bind(serverSocket , (struct sockaddr*)&serverAddress , sizeof(serverAddress));
