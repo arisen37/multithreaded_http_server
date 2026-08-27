d26a5becc70edb7d32a0396dad982e21e0213117
arisen37 <arisen2356@gmail.com>
2026-08-28 02:05:18 +0530

    added production rules for request line parsing

--- src/socket_util.cpp
+++ src/socket_util.cpp
@@ -1,6 +1,6 @@
 #include <http_server/socket_util.h>
 
-int socket_util::createAndBindSocket(int PORT1){
+int socket_util::createAndBindSocket(int PORT_NUMBER){
 
     //AF_INET -> IPV4 , SOCK_STREAM -> TCP Socket , 0 -> OS automatically selects protocol for the domain and socket type
     int serverSocket = socket(AF_INET , SOCK_STREAM , 0);
@@ -8,9 +8,10 @@ int socket_util::createAndBindSocket(int PORT1){
     //sockaddr_in is the class used to store serverAddress
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(PORT1);
+    serverAddress.sin_port = htons(PORT_NUMBER);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
+    //To add error handling and if bind is possible
     bind(serverSocket , (struct sockaddr*)&serverAddress , sizeof(serverAddress));
 
     return serverSocket;
